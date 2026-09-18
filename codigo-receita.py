62b8e484a79ea8b1d62ff6ecddfb424161e9cb96
santaylor <santaylor@gmail.com>
2026-09-18 16:22:56 -0300

    Modifica chamada da função que abre o arquivo d ereceita de bolo

--- codigo-receita.py
+++ codigo-receita.py
@@ -1,3 +1,5 @@
 #Esse codigo abre o arquivo e le seu conteudo
 
-f = open('receita-bolo-cenoura.txt', 'r')
+#f = open('receita-bolo-cenoura.txt', 'r')
+
+f = os.walk('receita-bolo-cenoura.txt', 'r')
